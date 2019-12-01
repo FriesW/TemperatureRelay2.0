dedcb6fb8c0e9f5b8ef2f612179d7bcd5668d7e1
FriesW <william.james.fries@gmail.com>
2019-11-30 21:17:30 -0600

    ConfigMode: refactor, add service key

--- Arduino/ConfigMode.cpp
+++ Arduino/ConfigMode.cpp
@@ -1,43 +1,81 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
+#include <DNSServer.h>
 #include "ConfigMode.h"
 #include "nonvol.h"
+#include "status.h"
 
+static IPAddress ap_ip(192, 168, 4, 1);
 static ESP8266WebServer server(80);
-
-static const char * ssid = "Temp Mon Config";
-static const char * pass = "temppass"; //TODO
+static DNSServer dns_server;
 
 static const char * webpage =
 "<html><body>"
 "<h1>Temperature Monitor Settings</h1>"
 "<form action='/' method='post'>"
-"Internet AP SSID: <input type='text' name='ssid' maxlength='32' value='%s'><br>"
-"Internet AP password: <input type='text' name='apsk' maxlength='32' value='%s'><br>"
-"Report to domain/host/ip: <input type='text' name='host' maxlength='32' value='%s'><br>"
+"Internet AP SSID: <input type='text' name='ssid' maxlength='31' value='%s'><br>"
+"Internet AP password: <input type='text' name='apsk' maxlength='31' value='%s'><br>"
+"Report to domain/host/ip: <input type='text' name='host' maxlength='31' value='%s'><br>"
 "Report to port: <input type='number' name='port' min='0' max='65535' value='%d'><br>"
+"Service key: <input type='text' name='key' maxlength='31' value='%s'><br>"
 "<input type='submit' value='Apply'>"
 "</form>"
 "</body></html>";
 
-static char temp_buf[750];
+static char temp_buf[850];
 
-static void handleRoot();
+static void handle_redirect();
+static void handle_root();
 
-void ConfigMode()
+_ConfigMode::_ConfigMode()
 {
-    WiFi.mode(WIFI_AP);
-    WiFi.softAP(ssid, pass);
-    server.on("/", handleRoot);
-    server.begin();
-    while(true)
-    {
-        server.handleClient();
-    }
-
+    running = false;
 }
 
-static void handleRoot()
+void _ConfigMode::start()
+{
+    running = true;
+
+    Status.all_blink();
+
+    WiFi.mode(WIFI_AP);
+    WiFi.softAPConfig(ap_ip, ap_ip, IPAddress(255, 255, 255, 0));
+    WiFi.softAP("Temp Sensor Config");
+
+    server.on("/", handle_root);
+    server.onNotFound(handle_redirect);
+    server.begin();
+    dns_server.start(53, "*", ap_ip);
+}
+
+void _ConfigMode::stop()
+{
+    if(!running)
+        return;
+
+    running = false;
+    dns_server.stop();
+    server.stop();
+
+    Status.all_off();
+}
+
+void _ConfigMode::periodic()
+{
+    if(!running)
+        return;
+
+    dns_server.processNextRequest();
+    server.handleClient();
+}
+
+static void handle_redirect()
+{
+    server.sendHeader("Location", "/",true);
+    server.send(307, "text/plain","redirecting...");
+}
+
+static void handle_root()
 {
     boolean modified = false;
     if( server.hasArg("ssid") ) { modified = true;
@@ -48,18 +86,22 @@ static void handleRoot()
         server.arg("host").toCharArray(Nonvol.data.host, HOST_LEN); }
     if( server.hasArg("port") ) { modified = true;
         Nonvol.data.port = server.arg("port").toInt(); }
+    if( server.hasArg("key") ) { modified = true;
+        server.arg("key").toCharArray(Nonvol.data.hmac_key, HKEY_LEN); }
 
     if( modified ) Nonvol.save();
 
     if( Nonvol.is_valid() )
         snprintf(
             temp_buf, sizeof(temp_buf), webpage,
-            Nonvol.data.ssid, Nonvol.data.apsk, Nonvol.data.host, Nonvol.data.port
+            Nonvol.data.ssid, Nonvol.data.apsk, Nonvol.data.host, Nonvol.data.port, Nonvol.data.hmac_key
         );
     else
     snprintf(
         temp_buf, sizeof(temp_buf), webpage,
-        "", "", "", 0
+        "", "", "", 0, ""
     );
     server.send(200, "text/html", temp_buf);
 }
+
+_ConfigMode ConfigMode;
--- Arduino/ConfigMode.h
+++ Arduino/ConfigMode.h
@@ -1,6 +1,24 @@
 #ifndef _CONFIGMODE_H
 #define _CONFIGMODE_H
 
-void ConfigMode();
+#include <Arduino.h>
+
+class _ConfigMode {
+
+public:
+    _ConfigMode();
+
+    void start();
+
+    void stop();
+
+    void periodic();
+
+private:
+    boolean running;
+
+};
+
+extern _ConfigMode ConfigMode;
 
 #endif
