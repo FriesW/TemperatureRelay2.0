fc632df32410ff381d9f9de434b4a0b95a5aeb95
FriesW <william.james.fries@gmail.com>
2019-12-11 17:53:44 -0600

    Basic packet handler
    Untested

--- server/main.py
+++ server/main.py
@@ -1,11 +1,41 @@
 import asyncio
 import packet
 import udp_server
+import database as db
+import time
 
 
 def handler(data):
-    print(packet.parse_packet(data))
-    return packet.make_ack_packet()
+    status, data = packet.parse_packet(data)
+    print(status, data)
+    if status:
+        epoch, temps = data
+        process(epoch, temps)
+        return packet.make_ack_packet()
+
+def process(epoch, temps):
+    if epoch == 0:
+        print("zeroed time")
+        return
+    if len(temps) == 0:
+        print("zero length temp list")
+        return
+    if epoch < time.time() - 60 * 60 * 24 * 15: #15 days
+        print("old time")
+        return
+
+    recent_t = 0
+    recent = db.most_recent()
+    if recent: recent_t, _ = recent
+
+    for t in temps:
+        if epoch < recent_t:
+            print("time {:,} before most recent entry at {:,}".format(epoch, recent_t))
+            continue
+        if not db.insert(epoch, t):
+            print("failed to insert:", epoch, t)
+        epoch += 60 * 10
+
 
 
 if __name__ == '__main__':
